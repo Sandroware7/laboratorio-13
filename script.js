4dcd977206705eeb3dac7ed5c45c02f773d4af6a
Sandroware7 <sandro.rodriguez.b@tecsup.edu.pe>
2024-11-20 23:03:26 -0500

    se borraron comentarios

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
 //Página 2
-// Función para validar los datos del formulario
 function validarFormulario() {
-    // Obtener los valores del formulario
+    
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
     const dni = document.getElementById('dni').value;
     const correo = document.getElementById('correo').value;
 
-    // Validar nombre
     if (nombre === "") {
         alert("Por favor ingrese su nombre.");
         return;
     }
 
-    // Validar apellido
     if (apellido === "") {
         alert("Por favor ingrese su apellido.");
         return;
     }
 
-    // Validar DNI con length
     if (dni.length !== 8 || isNaN(dni)) {
         alert("El DNI debe contener exactamente 8 caracteres numéricos.");
         return;
     }
 
-    // Validar correo electrónico (debe contener '@')
     if (!correo.includes("@")) {
         alert("El correo electrónico debe contener una '@'.");
         return;
     }
 
-    // Si todo es válido, mostrar mensaje de éxito
     alert("Datos ingresados con éxito");
 }
 //Página 3
